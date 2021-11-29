ed40e544d38e47e56df48dee1610fce403852c5b
aritra741 <aritra741@gmail.com>
2021-11-29 17:34:25 +0600

    POST added to product list endpoint

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -4,14 +4,25 @@ from decimal import Decimal
 from store.models import Product, Collection
 
 class CollectionSerializer(serializers.Serializer):
-    id= serializers.IntegerField()
-    title= serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
-    id= serializers.IntegerField()
-    title= serializers.CharField(max_length= 255)
-    budget= serializers.DecimalField(max_digits=8,decimal_places=2)
-    collection= CollectionSerializer()
+    class Meta:
+        model= Product
+        fields= ['id', 'title', 'budget', 'collection']
+
+    # id= serializers.IntegerField()
+    # title= serializers.CharField(max_length=255)
+
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model= Product
+        fields= ['id', 'title', 'budget', 'collection']
+    
+    # id= serializers.IntegerField()
+    # title= serializers.CharField(max_length= 255)
+    # budget= serializers.DecimalField(max_digits=8,decimal_places=2)
+    # collection= CollectionSerializer()
+    
     # budget_with_tax= serializers.SerializerMethodField(method_name='withTax')
 
     # def withTax(self, product: Product):
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -7,11 +7,19 @@ from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset= Product.objects.select_related('collection').all()
-    serializer= ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method=='GET':
+        queryset= Product.objects.select_related('collection').all()
+        serializer= ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer= ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response('ok')
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view()
 def product_detail(request, id):
